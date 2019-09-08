a251e4296f58881c14da27a9d2b39d869e25d7e9
clarymt <clarymt@me.com>
2019-09-08 11:36:25 -0400

    created some pseudocode and question ideas

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,97 @@
-console.log('pregunta')
+$(document).ready();
+//generate quiz questions by creating array
+//the create array for each question
+const questionSet = [
+    {
+        question:"Which WAS NOT a band name the group considered before 'Nirvana'?",
+        multipleChoice: ["Skid Row", "Pen Cap Chew", "Ted Ed Fred", "Sonic Youth"],
+        answer: "Sonic Youth"
+    },
+
+    {
+        question:'',
+        multipleChoice: [],
+        answer:
+    },
+
+    {
+        question:'',
+        multipleChoice: [],
+        answer:
+    },
+
+    {
+        question:'',
+        multipleChoice: [],
+        answer:
+    },
+
+    {
+        question:'',
+        multipleChoice: [],
+        answer:
+    },
+
+    {
+        question:'',
+        multipleChoice: [],
+        answer:
+    },
+
+    {
+        question:'',
+        multipleChoice: [],
+        answer:
+    },
+
+
+    {
+        question:'',
+        multipleChoice: [],
+        answer:
+    },
+
+    {
+        question:'',
+        multipleChoice: [],
+        answer:
+    },
+
+    {
+        question:'',
+        multipleChoice: [],
+        answer:
+    },
+];
+//Cobain and Novoselic were always members 
+//of Nirvana—formerly known 
+//as Skid Row, Pen Cap Chew, Bliss, and Ted Ed Fred
+
+//KURT COBAIN DROPPED OUT OF HIGH SCHOOL, 
+//THEN WORKED THERE AS A JANITOR.
+
+// NIRVANA'S FIRST ALBUM COST $606.17 TO MAKE.
+
+//SMELLS LIKE TEEN SPIRIT" WAS INSPIRED
+//BY COBAIN'S GIRLFRIEND'S DEODORANT. AND ALCOHOL
+
+//IN UTERO WAS INITIALLY GOING 
+//TO BE TITLED I HATE MYSELF AND I WANT TO DIE.
+
+//Nirvana’s most successful album, Nevermind
+//They sold 30 million
+
+//Nevermind had a budget of $65,000. 
+
+//kurt Cobain was inspired by a bit of graffiti 
+//written by his friend on the wall of his bedroom 
+//which read “Kurt smells like Teen Spirit
+
+//According to Krist Novoselic,
+//he and Cobain had started doing that out of 
+//frustration with a previous drummer, Chad Channing. 
+//Channing would often get songs wrong, 
+//which infuriated his fellow band members.
+
+//12. Kurt’s saliva on Elton John’s piano
+
