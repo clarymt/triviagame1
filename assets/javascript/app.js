3008c67a07e787f7f64684ebe0f27c1d68818645
clarymt <clarymt@me.com>
2019-09-08 12:56:55 -0400

    created question set and removed psuedocode of what they would be

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,95 +3,67 @@ $(document).ready();
 //the create array for each question
 const questionSet = [
     {
-        question:"Which WAS NOT a band name the group considered before 'Nirvana'?",
+        question: "Which WAS NOT a band name the group considered before 'Nirvana'?",
         multipleChoice: ["Skid Row", "Pen Cap Chew", "Ted Ed Fred", "Sonic Youth"],
         answer: "Sonic Youth"
     },
 
     {
-        question:'',
-        multipleChoice: [],
-        answer:
+        question: "Kurt Cobain dropped out of high school, and shortly after became a: ",
+        multipleChoice: ["Waiter.", "Janitor.", "Chef.", "Garbage Truck Driver."],
+        answer: "Janitor"
     },
 
     {
-        question:'',
-        multipleChoice: [],
-        answer:
+        question: "How much did Nirvanas first album, 'Bleach' cost to make?",
+        multipleChoice: ["$606.17", "$1504.69", "$250", "$2475.19"],
+        answer: "$606.17"
     },
 
     {
-        question:'',
-        multipleChoice: [],
-        answer:
+        question: "What was 'Smells Like Teen Spirit' insprired by?",
+        multipleChoice: ["Cobain's old girlfriend's deodorant", "Cigarettes and Alcohol", "Cobain when he was the school janitor", "The bands first concert at a local college"],
+        answer: "Cobain's old girlfriend's deodorant"
     },
 
     {
-        question:'',
-        multipleChoice: [],
-        answer:
+        question: "What was their 3rd album, 'In Utero' origininally titled?",
+        multipleChoice: ["Fetus Police", "All Apologies", "I Hate Myself and Want To Die", "Anatomny of a Heart-Shaped Box"],
+        answer: "I Hate Myself and Want To Die"
     },
 
     {
-        question:'',
-        multipleChoice: [],
-        answer:
+        question: "Nirvana's most successful album 'Nevermind' sold how many copies?",
+        multipleChoice: ["10 million", "20 million", "30 million", "40 million"],
+        answer: "30 million"
     },
 
     {
-        question:'',
-        multipleChoice: [],
-        answer:
+        question: "What was the budget for the making of 'Nevermind'?",
+        multipleChoice: ["$25,000", "65,000", "$85,000", "$105,000"],
+        answer: "$65,000"
     },
 
 
     {
-        question:'',
-        multipleChoice: [],
-        answer:
+        question: "What allegedly began Nirvana's notorious lust for destroying their set equipment?",
+        multipleChoice: ["One of their former Drummers consistently messing up", "Drugs", "The band being upset for Weird Al making fun of them"],
+        answer: "One of their former Drummers consistently messing up"
     },
 
     {
-        question:'',
-        multipleChoice: [],
-        answer:
+        question: "During the 1992 VMAs Cobain spat on a piano he thought was a part of Guns n Roses set, but it the piano was in fact played by ______.",
+        multipleChoice: ["David Bowie", "Ray Charles", "Bruce Springsteen", "Elton John"],
+        answer: "Elton John"
     },
 
     {
-        question:'',
-        multipleChoice: [],
-        answer:
+        question: "How many drummers came and went in the band before they found David Grohl, who would later create the Foo Fighters?",
+        multipleChoice: ["1", "2", "3", "4"],
+        answer: "4"
     },
 ];
-//Cobain and Novoselic were always members 
-//of Nirvana—formerly known 
-//as Skid Row, Pen Cap Chew, Bliss, and Ted Ed Fred
 
-//KURT COBAIN DROPPED OUT OF HIGH SCHOOL, 
-//THEN WORKED THERE AS A JANITOR.
 
-// NIRVANA'S FIRST ALBUM COST $606.17 TO MAKE.
 
-//SMELLS LIKE TEEN SPIRIT" WAS INSPIRED
-//BY COBAIN'S GIRLFRIEND'S DEODORANT. AND ALCOHOL
-
-//IN UTERO WAS INITIALLY GOING 
-//TO BE TITLED I HATE MYSELF AND I WANT TO DIE.
-
-//Nirvana’s most successful album, Nevermind
-//They sold 30 million
-
-//Nevermind had a budget of $65,000. 
-
-//kurt Cobain was inspired by a bit of graffiti 
-//written by his friend on the wall of his bedroom 
-//which read “Kurt smells like Teen Spirit
-
-//According to Krist Novoselic,
-//he and Cobain had started doing that out of 
-//frustration with a previous drummer, Chad Channing. 
-//Channing would often get songs wrong, 
-//which infuriated his fellow band members.
-
-//12. Kurt’s saliva on Elton John’s piano
 
