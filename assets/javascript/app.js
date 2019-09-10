0248e3cb22a7d913657d7e79e5170e22aeab33e4
clarymt <clarymt@me.com>
2019-09-09 22:47:37 -0400

    changed to display gif after question also made major css changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,62 +4,81 @@
         {
             triviaQuestion: "Which WAS NOT a band name the group considered before 'Nirvana'?",
             multipleChoice: ["Skid Row", "Pen Cap Chew", "Ted Ed Fred", "Sonic Youth"],
-            answer: "Sonic Youth"
+            answer: "Sonic Youth",
+            image: "./assets/images/image1.gif"
         },
 
         {
             triviaQuestion: "Kurt Cobain dropped out of high school, and shortly after became a: ",
             multipleChoice: ["Waiter", "Janitor", "Chef", "Garbage Truck Driver"],
-            answer: "Janitor"
+            answer: "Janitor",
+            image: "./assets/images/image2.gif"
+
         },
 
         {
             triviaQuestion: "How much did Nirvanas first album, 'Bleach' cost to make?",
             multipleChoice: ["$606.17", "$1504.69", "$250", "$2475.19"],
-            answer: "$606.17"
+            answer: "$606.17",
+            image: "./assets/images/image3.gif"
+
         },
 
         {
             triviaQuestion: "What was 'Smells Like Teen Spirit' insprired by?",
             multipleChoice: ["Cobain's old girlfriend's deodorant", "Cigarettes and Alcohol", "Cobain when he was the school janitor", "The bands first concert at a local college"],
-            answer: "Cobain's old girlfriend's deodorant"
+            answer: "Cobain's old girlfriend's deodorant",
+            image: "./assets/images/image4-.gif"
+
         },
 
         {
             triviaQuestion: "What was their 3rd album, 'In Utero' origininally titled?",
-            multipleChoice: ["Fetus Police", "All Apologies", "I Hate Myself and Want To Die", "Anatomny of a Heart-Shaped Box"],
-            answer: "I Hate Myself and Want To Die"
+            multipleChoice: ["Fetus Police", "All Apologies", "I Hate Myself and Want To Die", "Anatomy of a Heart-Shaped Box"],
+            answer: "I Hate Myself and Want To Die",
+            image: "./assets/images/image4.gif"
+
         },
 
         {
             triviaQuestion: "Nirvana's most successful album 'Nevermind' sold how many copies?",
             multipleChoice: ["10 million", "20 million", "30 million", "40 million"],
-            answer: "30 million"
+            answer: "30 million",
+            image: "./assets/images/image5.gif"
+
         },
 
         {
             triviaQuestion: "What was the budget for the making of 'Nevermind'?",
             multipleChoice: ["$25,000", "$65,000", "$85,000", "$105,000"],
-            answer: "$65,000"
+            answer: "$65,000",
+            image: "./assets/images/image6.gif"
+
         },
 
 
         {
             triviaQuestion: "What allegedly began Nirvana's notorious lust for destroying their set equipment?",
             multipleChoice: ["One of their former Drummers consistently messing up", "Drugs", "The band being upset for Weird Al making fun of them"],
-            answer: "One of their former Drummers consistently messing up"
+            answer: "One of their former Drummers consistently messing up",
+            image: "./assets/images/image7.gif"
+
         },
 
         {
             triviaQuestion: "During the 1992 VMAs Cobain spat on a piano he thought was a part of Guns n Roses set, but it the piano was in fact played by ______.",
             multipleChoice: ["David Bowie", "Ray Charles", "Bruce Springsteen", "Elton John"],
-            answer: "Elton John"
+            answer: "Elton John",
+            image: "./assets/images/image8.gif"
+
         },
 
         {
             triviaQuestion: "How many drummers came and went in the band before they found David Grohl, who would later create the Foo Fighters?",
             multipleChoice: ["1", "2", "3", "4"],
-            answer: "4"
+            answer: "4",
+            image: "./assets/images/image9.gif"
+
         },
     ];
 
@@ -90,7 +109,8 @@
         function timeStop() {
             clearInterval(timer);
             lost++;
-            questionNext();
+            $("#game").html('<p> Correct answer: ' + questionSet[currQuestion].answer + '</p> <img src=' + questionSet[currQuestion].image + '> </img>');
+            setTimeout(questionNext, 3000);
         }
 
         function countDown(){
@@ -133,15 +153,18 @@ $(document).on('click', '.choice', function() {
 
     if (correctChoice === answerChoice) {
         score++;
-        questionNext ();
+        $("#game").html('<p> Correct answer: ' + correctChoice + '</p> <img src=' + questionSet[currQuestion].image + '> </img>');
+        setTimeout(questionNext, 3000);
         //console.log("yes")
     }
 
     else{
         lost ++;
-        questionNext ();
+        $("#game").html('<p> Correct answer: ' + correctChoice + '</p> <img src=' + questionSet[currQuestion].image + '> </img>');
+        setTimeout(questionNext, 3000);
         //console.log("no")
     }
+
     //console.log('clicky clicky picky clicky', answerChoice);
 });
 
@@ -159,7 +182,7 @@ function summary() {
 }
 
 $(document).on('click','#reset',function() {
-    count = 25;
+    count = 2;
     currQuestion = 0;
     score = 0;
     lost = 0;
@@ -172,7 +195,7 @@ $(document).on('click','#reset',function() {
 function remainingQuestions() {
     const remainQuestion = questionSet.length - (currQuestion);
     const totalQuestions = questionSet.length;
-    return `Remaining Questions: ${remainQuestion}/${totalQuestions}`;
+    return `<p class='left'>Remaining Questions: ${remainQuestion}/${totalQuestions}</p>`;
 }
 
 loadQuestion();
